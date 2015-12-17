351c973b894c6b500a1df04f9856ffc98da6531a
fabianormatias <fabianormatias@gmail.com>
2015-12-17 11:04:34 -0200

    primeiro commit

---
+++ ws-cookbook/src/main/java/com/g6/ex1/server/TimeServerImpl.java
@@ -0,0 +1,22 @@
+package com.g6.ex1.server;
+
+import java.util.Date;
+
+import javax.jws.WebService;
+
+/**
+ * The @WebService property endpointInterface links the
+ * SIB (this class) to the SEI (ch01.ts.TimeServer).
+ * Note that the method implementations are not annotated
+ * as @WebMethods.
+*/
+@WebService(endpointInterface = "com.g6.ex1.server.TimeServer")
+public class TimeServerImpl implements TimeServer {
+	public String getTimeAsString() {
+		return new Date().toString();
+	}
+
+	public long getTimeAsElapsed() {
+		return new Date().getTime();
+	}
+}
---
+++ ws-cookbook/src/main/java/com/g6/ex3/server/Teams.java
@@ -0,0 +1,26 @@
+package com.g6.ex3.server;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class Teams {
+	private TeamsUtility utils;
+
+	public Teams() {
+		utils = new TeamsUtility();
+		utils.make_test_teams();
+	}
+
+	@WebMethod
+	public Team getTeam(String name) {
+		return utils.getTeam(name);
+	}
+
+	@WebMethod
+	public List<Team> getTeams() {
+		return utils.getTeams();
+	}
+}
